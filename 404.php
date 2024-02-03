21511cd7a913da87f197459622a8bb2fd7a7b46a
danelian <141317269+danelian@users.noreply.github.com>
2024-02-03 20:28:30 +0300

    page 404 - completed

--- 404.php
+++ 404.php
@@ -3,8 +3,8 @@
 <div class="error-section">
   <div class="container">
     <span class="subtitle">404</span>
-    <h1 class="title">Такой страницы <br> не существует</h1>
-    <a href="/" class="button-blur">На главную</a>
+    <h1 class="title"><?php echo __('This page', 'freshauto'); ?><br><?php echo __('does not exist', 'freshauto'); ?></h1>
+    <a href="<?php echo get_home_url(); ?>" class="button-blur"><?php echo __('To main', 'trophy') ?></a>
   </div>
 </div>
 
